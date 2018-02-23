e6c71fc2756f20530d5c8c55784320846967f0d4
Daniel Ivert <ivert.daniel@gmail.com>
2018-02-23 20:55:47 +0100

    Revert "start adding Animation Queue"
    This reverts commit dad91e280c3a0cc563dd0d29bab7d3906a08a473.

--- EffectLED/TimerQueue.h
+++ EffectLED/TimerQueue.h
@@ -11,8 +11,7 @@
 
 
 #include <LinkedList.h>
-#define micsTOs 1000 * 1000
-#define sTOmics 1 / msTOs
+
 
 class TimerQueueClass
 {
@@ -35,35 +34,10 @@ class TimerQueueClass
 	 };
     
     struct AnimationTask {
-		void (*_task)(float)
-        unsigned long lastExecuted;
-        unsigned long endTimeMicros;
-        float startValue, endValue, actValue;
-        float duration;
-        float timeDone;
-        AnimationTask(void (*task)(float), float start, float end, float dur){
-			_task = task;
-			startValue = start;
-			actValue = start;
-			endValue = end;
-			duration = dur;
-			endTimeMicros = micros() + (unsigned long)(duration * sTOmics);
-			timeDone = 0;
-			lastExecuted = micros();
-        }
-        //returns whether animation iscompleted
-        bool execute() {
-			unsigned long time = micros();
-			if (time > endTimeMicros){
-				_task(endValue)
-				return true
-			}
-			float deltaTime = (float)(time - lastExecuted) * micsTOs
-			actValue += deltaTime / duration * (endValue - startValue)
-			_task(actValue)
-			lastExecuted = time;
-			return false;
-        }
+       	(*task)(value)
+         unsigned long lastExecuted;
+      float
+		//	start und end value
 		//	speed/ duration
 		//	wenn executed
     }
